c7d184e8ecf096564741d4fb05e65e9a2e3746b8
Tarik Djebien <tarik@MacBookPro.local>
2013-04-30 10:45:56 +0200

    Initial Commit

---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by Tarik Djebien on 30/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
---
+++ Psychologist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Psychologist
+//
+//  Created by Tarik Djebien on 30/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PsychologistAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PsychologistAppDelegate class]));
+    }
+}
