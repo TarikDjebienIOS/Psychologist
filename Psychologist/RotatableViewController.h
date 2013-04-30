8323d8d92150027817b9e431adc1e9b432b70fad
Tarik Djebien <tarik@pc12047.osyris-sa-industrie.local>
2013-04-30 16:46:07 +0200

    Add support orientation, iPad and iPhone storyboard

---
+++ Psychologist/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Psychologist
+//
+//  Created by Tarik Djebien on 30/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ Psychologist/RotatableViewController.m
@@ -0,0 +1,18 @@
+//
+//  RotatableViewController.m
+//  Psychologist
+//
+//  Created by Tarik Djebien on 30/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@implementation RotatableViewController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
